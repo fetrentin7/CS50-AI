e5b27e34126b81de2de88bda74396cb02fd8c220
fetrentin7 <fetrentin7@gmail.com>
2025-05-07 16:47:38 -0300

    running

--- week0/tictactoe/tictactoe.py
+++ week0/tictactoe/tictactoe.py
@@ -80,17 +80,17 @@ def checkLine(board):
 
     for row in range(3):
         if board[row][0] == board[row][1] == board[row][2] != EMPTY:
-            return [row][1]
+            return board[row][1]
 
     return None
 
 def checkDiagonal(board):
 
     if board[0][0] == board[1][1] == board[2][2] != EMPTY:
-       return [0][0]
+       return board[0][0]
 
     if board[0][2] == board[1][1] == board[2][0] != EMPTY:
-       return [0][2]
+       return board[0][2]
 
     return None
 
@@ -98,20 +98,34 @@ def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    raise NotImplementedError
+    return checkLine(board) or checkColumn(board) or checkDiagonal(board)
 
 
 def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
-    print(board)
+    if winner(board) is not None:
+        return True
+
+    for row in range(3):
+        for column in range(3):
+            if board[row][column] == EMPTY:
+                return False
+    return False
 
 def utility(board):
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
-    raise NotImplementedError
+    winner_player = winner(board)
+
+    if winner_player == X:
+        return 1
+    elif winner_player == O:
+        return -1
+    else:
+        return 0
 
 
 def minimax(board):
